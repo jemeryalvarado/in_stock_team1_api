b98432225f99f0a63038e0d600964362846ea7c1
Brandon <brandon.redford@outlook.com>
2024-05-11 10:29:36 -0700

    add router.GET for a single warehouse to warehouses.js router

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,9 +1,20 @@
+const knex = require('knex')(require('../knexfile'));
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("this is awesomeness");
+  router.get('/:id', async (_req, res) => {
+    try {
+      const data = await knex('warehouses');
+      const warehouseId = _req.params.id;
+      const warehouse = data.find(warehouse => warehouse.id == warehouseId)
+      if (warehouse) {
+        res.status(200).json(warehouse)
+      } else {
+        res.status(404).json(`Warehouse id: ${warehouseId} does not exist`)
+      }
+    } catch(err) {
+      res.status(400).send(`Error retrieving Users: ${err}`)
+    }
   });
-  
 
 module.exports = router;
