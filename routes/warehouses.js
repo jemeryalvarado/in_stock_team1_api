f7a690dcf77747c6bdbb35fef8b1be4fafecfee4
Arian Faridi <arianfaridi@Arians-MBP.home>
2024-05-11 13:01:11 -0700

    get request for all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -2,6 +2,17 @@ const knex = require('knex')(require('../knexfile'));
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async(_req, res) => {
+  try{
+    const all_warehouses = await knex('warehouses');
+    const all_warehouses_noTimeStamps= all_warehouses.map(({created_at, updated_at, ...cleanedData})=>cleanedData); 
+    res.status(200).json(all_warehouses_noTimeStamps);
+  }catch{
+    res.status(500).send('Error getting warehouses');
+  }
+  });
+
+
   router.get('/:id', async (_req, res) => {
     try {
       const data = await knex('warehouses');
