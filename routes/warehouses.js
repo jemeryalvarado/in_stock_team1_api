3cc108ec49b749230cfb8aad74f011d1bc1fbf73
Arian Faridi <arianfaridi@Arians-MBP.home>
2024-05-11 12:05:50 -0700

    filter out timestamps from the data

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -5,7 +5,8 @@ const router = express.Router();
 router.get("/", async(_req, res) => {
   try{
     const all_warehouses = await knex('warehouses');
-    res.status(200).json(all_warehouses);
+    const all_warehouses_noTimeStamps= all_warehouses.map(({created_at, updated_at, ...cleanedData})=>cleanedData); 
+    res.status(200).json(all_warehouses_noTimeStamps);
   }catch{
     res.status(500).send('Error getting warehouses');
   }
