37a693ed498cb24a82da2b9d0e90cbc34363c2bf
Dina Shouhdy <dshouhdy@gmail.com>
2024-05-11 11:50:07 -0700

    created PUT API request for warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("this is awesomeness");
-  });
-  
+router.put('/:id', async(_req, res) => {
+  const warehouseId = _req.params.id;
+  const warehouse = data.find(warehouse => warehouse.id == warehouseId);
+  const result = await warehouse.replaceOne({_id: warehouseId}, _req.body);
+  console.log(result);
+
+  if (warehouse) {
+    res.status(200).json(warehouse)
+  } else {
+    res.status(404).json(`New warehouse ${warehouseId} is not updated.`)
+  }
+  res.json({updatedCount: result.modifiedCount});
+});
 
 module.exports = router;
