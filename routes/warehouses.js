4f0f517466dff4d0589ae095f7e47b6e560dfc46
Dina Shouhdy <dshouhdy@gmail.com>
2024-05-11 13:16:26 -0700

    updated PUT request ini warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -6,14 +6,14 @@ router.put('/:id', async(_req, res) => {
   try {
     const warehouseId = _req.params.id;
     const data = {
-      "warehouse_name": "Brooklyn",
-      "address": "918 Morris Lane",
-      "city": "Brooklyn",
-      "country": "USA",
-      "contact_name": "Parmin Aujla",
-      "contact_position": "Warehouse Manager",
-      "contact_phone": "+1 (646) 123-1234",
-      "contact_email": "paujla@instock.com"
+      warehouse_name: "Brooklyn",
+      address: "918 Morris Lane",
+      city: "Brooklyn",
+      country: "USA",
+      contact_name: "Parmin Aujla",
+      contact_position: "Warehouse Manager",
+      contact_phone: "+1 (646) 123-1234",
+      contact_email: "paujla@instock.com"
     };
 
     const result = await knex('warehouses').where({ id: warehouseId }).update(_req.body);
