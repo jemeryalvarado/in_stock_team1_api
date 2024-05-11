c0b6d994d11e5f5d4dbf6fc2a901e865b0621574
Arian Faridi <arianfaridi@Arians-MBP.home>
2024-05-11 11:47:51 -0700

    first iteration of get for all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,8 +1,14 @@
+const knex = require('knex')(require('../knexfile'));
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("this is awesomeness");
+router.get("/", async(_req, res) => {
+  try{
+    const all_warehouses = await knex('warehouses');
+    res.status(200).json(all_warehouses);
+  }catch{
+    res.status(500).send('Error getting warehouses');
+  }
   });
   
 
