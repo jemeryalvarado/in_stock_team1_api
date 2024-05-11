d0dc3e7bb9097d815b5ef0258509c600752655ac
Arian Faridi <arianfaridi@Arians-MBP.home>
2024-05-11 12:54:01 -0700

    resovled the conflicts

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -11,6 +11,21 @@ router.get("/", async(_req, res) => {
     res.status(500).send('Error getting warehouses');
   }
   });
-  
 
+  
+  router.get('/:id', async (_req, res) => {
+    try {
+      const data = await knex('warehouses');
+      const warehouseId = _req.params.id;
+      const warehouse = data.find(warehouse => warehouse.id == warehouseId)
+      if (warehouse) {
+        const { created_at, updated_at, ...warehouseWithoutTimeStamps } = warehouse;
+        res.status(200).json(warehouseWithoutTimeStamps)
+      } else {
+        res.status(404).json(`Warehouse id: ${warehouseId} does not exist`)
+      }
+    } catch(err) {
+      res.status(400).send(`Error retrieving Users: ${err}`)
+    }
+  });
 module.exports = router;
