efa204841121f31ac378c03349574bdf101d8e01
Brandon <brandon.redford@outlook.com>
2024-05-12 07:05:57 -0700

    merge develop into new feature branch

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,9 +1,21 @@
+const knex = require('knex')(require('../knexfile'));
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("this is awesomeness");
+router.put('/:id', async (_req, res) => {
+    try {
+      const data = await knex('inventories');
+      const inventoryId = _req.params.id;
+      const inventory = data.find(inventory => inventory.id == inventoryId)
+      if (inventory) {
+        const { created_at, updated_at, ...warehouseWithoutTimeStamps } = warehouse;
+        res.status(200).json(warehouseWithoutTimeStamps)
+      } else {
+        res.status(404).json(`Warehouse id: ${warehouseId} does not exist`)
+      }
+    } catch(err) {
+      res.status(400).send(`Error retrieving Users: ${err}`)
+    }
   });
-  
 
 module.exports = router;
